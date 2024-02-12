402c37d3863c8504c9788fbdc586ac75e18f801f
Naveen Kamagoud <136416909+naveenmk404@users.noreply.github.com>
2024-02-12 23:24:30 +0530

    Update p5.py

--- p5.py
+++ p5.py
@@ -1,74 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class NeuralNetwork:
-    def __init__(self, input_size, output_size, hidden_size):
-        self.input_size = input_size
-        self.output_size = output_size
-        self.hidden_size = hidden_size
+def quadratic_function(x):
+    return x**2-2*x
 
-        self.hidden_layer_input_weights = np.random.rand(self.hidden_size, self.input_size)
-        self.hidden_layer_bias = np.zeros((self.hidden_size, 1))
-        self.hidden_layer_output_weights = np.random.rand(self.output_size, self.hidden_size)
-
-        self.output_bias = np.zeros((self.output_size, 1))
-        self.learning_rate = 0.01
-
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
-
-    def train(self, inputs, target, epochs):
-        losses = []
-
-        for epoch in range(epochs):
-            hidden_layer_input = np.dot(self.hidden_layer_input_weights, inputs) + self.hidden_layer_bias
-            hidden_layer_output = self.sigmoid(hidden_layer_input)
-
-            output_layer_input = np.dot(self.hidden_layer_output_weights, hidden_layer_output) + self.output_bias
-            output_layer_output = self.sigmoid(output_layer_input)
-
-            error = target - output_layer_output
-            losses.append(np.mean(error**2))
-
-            output_layer_error = error * self.sigmoid_derivative(output_layer_output)
-            hidden_layer_error = np.dot(self.hidden_layer_output_weights.T, output_layer_error) * self.sigmoid_derivative(hidden_layer_output)
-
-            self.hidden_layer_output_weights += self.learning_rate * np.dot(output_layer_error, hidden_layer_output.T)
-            self.output_bias += self.learning_rate * np.sum(output_layer_error, axis=1, keepdims=True)
-
-            self.hidden_layer_input_weights += self.learning_rate * np.dot(hidden_layer_error, inputs.T)
-            self.hidden_layer_bias += self.learning_rate * np.sum(hidden_layer_error, axis=1, keepdims=True)
-
-        return losses
-
-    def predict(self, inputs):
-        hidden_inputs = np.dot(self.hidden_layer_input_weights, inputs) + self.hidden_layer_bias
-        hidden_output = self.sigmoid(hidden_inputs)
-
-        output_input = np.dot(self.hidden_layer_output_weights, hidden_output) + self.output_bias
-        output_output = self.sigmoid(output_input)
-
-        return output_output
+x_train = np.linspace(-1, 1, 100).reshape(-1, 1)
+y_train = quadratic_function(x_train)
 
 np.random.seed(42)
-X = np.random.rand(2, 100)
-y = (X[0, :] + X[1, :]).reshape(1, -1)
+weights_output = np.random.randn(1, 1)
+biases_output = np.zeros((1, 1))
+learning_rate = 0.01
+epochs = 1000
 
-X = (X - np.mean(X)) / np.std(X)
-y = (y - np.mean(y)) / np.std(y)
+for epoch in range(epochs):
+    output_layer_input = np.dot(x_train, weights_output) + biases_output
+    loss = np.mean((output_layer_input - y_train)**2)
+    output_error = output_layer_input - y_train
+    output_delta = output_error
+    weights_output -= learning_rate * np.dot(x_train.T, output_delta)
+    biases_output -= learning_rate * np.sum(output_delta, axis=0, keepdims=True)
+    if epoch % 100 == 0:
+        print(f"Epoch {epoch}, Loss: {loss}")
 
-input_size = X.shape[0]
-hidden_size = 4
-output_size = 1
-
-nn = NeuralNetwork(input_size, output_size, hidden_size)
-losses = nn.train(X, y, 1000)
-
-plt.plot(losses)
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.title('Training Loss over Epochs')
+predictions = np.dot(x_train, weights_output) + biases_output
+plt.scatter(x_train, y_train, label='Training Data')
+plt.plot(x_train, predictions, color='red', label='Model Prediction')
+plt.legend()
 plt.show()
